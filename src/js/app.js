b9a6796aafe902ce135e14645029dea372f0d46b
shahrizoda <shahrizodadeveloping@gmail.com>
2022-04-18 00:39:40 +0500

    initial

--- src/js/app.js
+++ src/js/app.js
@@ -5,3 +5,40 @@ functions.isWebp();
 // import Swiper, { Navigation, Pagination } from 'swiper';
 
 // const swiper = new Swiper();
+
+import IMask from 'imask';
+
+var phoneMask = IMask(
+    document.getElementById('phone'), {
+      mask: '+{7}(000)000-00-00'
+    });
+
+const dropBtn = document.querySelector('.dropdown__top');
+const dropdown = document.querySelector('.dropdown');
+dropBtn.addEventListener('click', () => {
+    dropBtn.classList.toggle('active');
+})
+document.addEventListener('click', e => {
+    if (!e.composedPath().includes(dropdown)){
+        dropBtn.classList.remove('active');
+    }
+})
+
+const btn = document.querySelector('.popup-open');
+const popup = document.querySelector('.popup');
+const popupClose = document.querySelector('.popup__close');
+const popupCloseIcon = document.querySelector('.popup__icon');
+const body = document.querySelector('body');
+
+btn.addEventListener('click', (e)=>{
+    popup.classList.toggle('active')
+    body.classList.toggle('active')
+})
+popupClose.addEventListener('click', (e)=>{
+    popup.classList.remove('active')
+})
+popupCloseIcon.addEventListener('click', (e)=>{
+    popup.classList.remove('active')
+})
+
+
